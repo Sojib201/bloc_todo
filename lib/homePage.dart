f38cea4af2c6b90d2d89118967b92fa28311958d
Abu Noman Basar <abunomanbasar.eappair@gmail.com>
2024-12-21 17:24:14 +0600

    dsfgdfvds

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -13,87 +13,96 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   TextEditingController _controller = TextEditingController();
-  //final todoBloc = context.read<TodoBloc>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: BlocBuilder<TodoBloc, TodoState>(
         builder: (context, state) {
-      if (state is TodoUpdate) {
-        final todos = state.todos;
-
-        return ListView.builder(
-          itemCount: todos.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(todos[index]),
-              trailing: IconButton(
-                icon: const Icon(Icons.delete, color: Colors.red),
-                onPressed: () {
-                  context.read<TodoBloc>().add(deleteTodo(index));
-                },
-              ),
-            );
-          },
-        );
-      }
-      return const Center(child: Text('No todos available.'));
-    }));
-
+          if (state is TodoInit) {
+            return _view(context, '');
+          }
+          if (state is TodoUpdate) {
+            return _view(context, state.todos[]);
+          }
+          return Container();
+        },
+      ),
+    );
+    // return Scaffold(
+    //     body: BlocBuilder<TodoBloc, TodoState>(builder: (context, state) {
+    //   if (state is TodoUpdate) {
+    //     final todos = state.todos;
+    //
+    //     return ListView.builder(
+    //       itemCount: todos.length,
+    //       itemBuilder: (context, index) {
+    //         return ListTile(
+    //           title: Text(todos[index]),
+    //           trailing: IconButton(
+    //             icon: const Icon(Icons.delete, color: Colors.red),
+    //             onPressed: () {
+    //               context.read<TodoBloc>().add(deleteTodo(index));
+    //             },
+    //           ),
+    //         );
+    //       },
+    //     );
+    //   }
+    //   return const Center(child: Text('No todos available.'));
+    // }));
   }
 
-  // Widget _view(BuildContext context,int value){
-  //   return Column(
-  //     children: [
-  //       TextField(
-  //         controller: _controller,
-  //       ),
-  //       SizedBox(
-  //         width: 200,
-  //         child: ElevatedButton(
-  //           onPressed: () {
-  //             context.read<TodoBloc>().add(addTodo(_controller.text));
-  //             _controller.text='';
-  //           },
-  //           child: Text('Add ToDo'),
-  //         ),
-  //       ),
-  //       Expanded(
-  //         child: BlocBuilder<TodoCubit, TodoInitial>(
-  //           builder: (context, state) {
-  //             return ListView.builder(
-  //               itemCount: state.todos.length,
-  //               itemBuilder: (context, index) {
-  //                 return Card(
-  //                   elevation: 5,
-  //                   child: Column(
-  //                     children: [
-  //                       Row(
-  //                         children: [
-  //                           Expanded(child: Text(state.todos[index])),
-  //                           IconButton(
-  //                             onPressed: () {
-  //                               context.read<TodoCubit>().DeleteToDo(index);
-  //                             },
-  //                             icon: Icon(
-  //                               Icons.delete,
-  //                               color: Colors.red,
-  //                             ),
-  //                           ),
-  //                         ],
-  //                       ),
-  //                     ],
-  //                   ),
-  //                 );
-  //               },
-  //             );
-  //           },
-  //         ),
-  //       ),
-  //     ],
-  //   )
-  // }
+  Widget _view(BuildContext context,String todos){
+    return Column(
+      children: [
+        TextField(
+          controller: _controller,
+        ),
+        SizedBox(
+          width: 200,
+          child: ElevatedButton(
+            onPressed: () {
+              context.read<TodoBloc>().add(addTodo(_controller.text));
+              _controller.text='';
+            },
+            child: Text('Add ToDo'),
+          ),
+        ),
+        Expanded(
+          child: BlocBuilder<TodoBloc, TodoState>(
+            builder: (context, state) {
+              return ListView.builder(
+                itemCount: todos.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 5,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(child: Text(todos[index]),),
+                            IconButton(
+                              onPressed: () {
+                                context.read<TodoBloc>().add(deleteTodo(index));
+                              },
+                              icon: Icon(
+                                Icons.delete,
+                                color: Colors.red,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
 }
